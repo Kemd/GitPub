5106b73e019845c9bc9fcba8d9ff90245f7b4944
kemd <kemd91@gmail.com>
2022-05-17 20:18:27 -0400

    added foods index / show ejs files, updated show with content

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // CONSTANTS
 const express = require('express')
 const drinks = require('./models/drinks')
+const food = require('./models/foods')
 
 const app = express()
 
@@ -20,6 +21,17 @@ app.get('/drinks', (req, res) => {
 app.get('/drinks/:id', (req, res) => {
     res.render('drinks_show.ejs', {
         drink: drinks[req.params.id]
+    })
+})
+
+// FOOD ROUTE
+app.get('/foods', (req, res) => {
+    res.render('food_index.ejs', { allFoods: food })
+})
+
+app.get('/foods/:foo', (req, res) => {
+    res.render('food_show.ejs', {
+        foodie: food[req.params.foo]
     })
 })
 
